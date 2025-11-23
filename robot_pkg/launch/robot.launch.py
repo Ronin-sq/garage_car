a3a4cd147fa4a3dc6d055b35c077737b2ff0d8b9
Ronin-sq <2097802563@qq.com>
2025-11-23 17:59:02 +0800

    ``` feat(robot_pkg): update launch file to use xacro and joint state publisher
    - Replace urdf file loading with xacro command for robot description
    - Change joint_state_publisher_gui to joint_state_publisher
    - Add rviz configuration file argument
    - Include joint state publisher node in launch description
    ```

--- robot_pkg/launch/robot.launch.py
+++ robot_pkg/launch/robot.launch.py
@@ -2,33 +2,37 @@ import launch
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
+from launch.substitutions import Command
 
 
 def generate_launch_description():
 
     package_path = get_package_share_directory("robot_pkg")
     urdf_file = os.path.join(package_path, "urdf", "robot.urdf")
-
+    xacro_file = os.path.join(package_path, "urdf", "robot.xacro")
+    ronin_robot_file = os.path.join(package_path, "urdf", "robot","robot.urdf.xacro")
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='robot_state_publisher',
-        parameters=[{'robot_description': open(urdf_file).read()}],
+        parameters=[{'robot_description': Command(['xacro ', ronin_robot_file])}],
     )
     joint_state_publisher_node = Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        name='joint_state_publisher_gui',
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
         output='screen',
     )
     rviz_node = Node(
         package='rviz2',
         executable='rviz2',
         name='rviz2',
+        arguments=['-d', os.path.join(package_path, 'config', 'robot.rviz')],
         output='screen',
     )
 
     return launch.LaunchDescription([
         robot_state_publisher_node,
+        joint_state_publisher_node,
         rviz_node
     ])
